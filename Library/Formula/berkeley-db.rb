d9025273c7d38ac2312c4206069b257f2ede8a8a
Giulio Eulisse <giulio.eulisse@cern.ch>
2010-08-13 09:34:00 +0200

    Allow disabling java and c++ api.

--- Library/Formula/berkeley-db.rb
+++ Library/Formula/berkeley-db.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class BerkeleyDb <Formula
-  @url='http://download.oracle.com/berkeley-db/db-5.0.21.tar.gz'
+  @url='http://download.oracle.com/berkeley-db/db-4.5.20.tar.gz'
   @homepage='http://www.oracle.com/technology/products/berkeley-db/index.html'
-  @md5='9a749fd2e98fe15840493ddc34cc66d8'
+  @md5='b0f1c777708cb8e9d37fb47e7ed3312d'
 
   aka 'db'
 
@@ -13,13 +13,12 @@ class BerkeleyDb <Formula
     ENV.O3 # takes an hour or more with link time optimisation
 
     # BerkeleyDB requires you to build everything from the build_unix subdirectory
+    configure_args = "--disable-debug",  "--prefix=#{prefix}", "--mandir=#{man}"
+    configure_args << "--enable-cxx" if ARGV.include? '--enable-cxx'
+    configure_args << "--enable-java" if ARGV.include? '--enable-java'
+    
     Dir.chdir 'build_unix' do
-      system "../dist/configure", "--disable-debug",
-                                  "--prefix=#{prefix}",
-                                  "--mandir=#{man}",
-                                  "--enable-cxx",
-                                  "--enable-java"
-
+      system "../dist/configure", *configure_args
       system "make install"
 
       # use the standard docs location
