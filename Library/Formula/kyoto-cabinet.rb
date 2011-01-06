eadfc366ecc70f8b440f11d7e93aba3711820187
Giulio Eulisse <giulio.eulisse@cern.ch>
2011-01-06 18:18:48 +0100

    Updated to 1.2.33

--- Library/Formula/kyoto-cabinet.rb
+++ Library/Formula/kyoto-cabinet.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class KyotoCabinet <Formula
-  url 'http://fallabs.com/kyotocabinet/pkg/kyotocabinet-1.2.26.tar.gz'
+  url 'http://fallabs.com/kyotocabinet/pkg/kyotocabinet-1.2.33.tar.gz'
   homepage 'http://fallabs.com/kyotocabinet/'
   md5 'ac0e13208b5391414462d89263227d24'
 
